884b4f0d1a72a5d75be53cb9383ccecbbd9db194
adhetyanugroho <adhetyanugroho@gmail.com>
2023-06-05 17:15:26 +0700

    update

--- db/seeders/20220528064914-users.js
+++ db/seeders/20220528064914-users.js
@@ -33,7 +33,7 @@ module.exports = {
       name,
       email: `${name.toLowerCase()}@binar.co.id`,
       encryptedPassword,
-      roleId: name.toLowerCase() === 'Jayabaya' ? roleAdmin.id : roleCustomer.id,
+      roleId: name.toLowerCase() === 'jayabaya' ? roleAdmin.id : roleCustomer.id,
       createdAt: timestamp,
       updatedAt: timestamp,
     }));
